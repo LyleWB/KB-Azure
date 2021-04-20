3a3087af77d9d63f1565a16e40df5f01dd488f69
LyleWB <lyle@connectds.com>
2021-04-20 15:46:36 +0100

    Create Size-Up-Hostpool.ps1

---
+++ Size-Up-Hostpool.ps1
@@ -0,0 +1,12 @@
+#SCALE UP SIZE
+##Kingston Burrowes Variables
+$resourceGroup = "WVD"
+$vmName = "KB-WVD-HPv4-0.ad.kingstonaccountants.co.uk"
+
+##List available VM sizes (optional)
+Get-AzVMSize -ResourceGroupName $resourceGroup -VMName $vmName
+
+##Re-Size VM to B2ms
+$vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
+$vm.HardwareProfile.VmSize = "Standard_D8s_v3"
+Update-AzVM -VM $vm -ResourceGroupName $resourceGroup
