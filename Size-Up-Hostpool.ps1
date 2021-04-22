b31d627baa265a3d29e3cda271022e34675d55b6
LyleWB <82891371+LyleWB@users.noreply.github.com>
2021-04-22 20:30:03 +0100

    Update Size-Up-Hostpool.ps1

--- Size-Up-Hostpool.ps1
+++ Size-Up-Hostpool.ps1
@@ -3,7 +3,7 @@
 $resourceGroup = "WVD"
 $vmName = "KB-WVD-HPv4-0"
 
-##Re-Size VM to B2ms
+##Re-Size VM to D8s
 $vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
 $vm.HardwareProfile.VmSize = "Standard_D8s_v3"
 Update-AzVM -VM $vm -ResourceGroupName $resourceGroup
