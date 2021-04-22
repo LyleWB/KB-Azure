ff27bdddac5be14e430f5cd25460adf92fbc37ea
LyleWB <82891371+LyleWB@users.noreply.github.com>
2021-04-22 11:18:28 +0100

    Update Size-Up-Hostpool.ps1
    Removed the list vm sizes command

--- Size-Up-Hostpool.ps1
+++ Size-Up-Hostpool.ps1
@@ -3,9 +3,6 @@
 $resourceGroup = "WVD"
 $vmName = "KB-WVD-HPv4-0.ad.kingstonaccountants.co.uk"
 
-##List available VM sizes (optional)
-Get-AzVMSize -ResourceGroupName $resourceGroup -VMName $vmName
-
 ##Re-Size VM to B2ms
 $vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
 $vm.HardwareProfile.VmSize = "Standard_D8s_v3"
