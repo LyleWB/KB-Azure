7884e860aa923cb5804650ab6361e1518c7d7731
LyleWB <82891371+LyleWB@users.noreply.github.com>
2021-05-13 09:42:54 +0100

    Update Size-Up-Hostpool.ps1
    Changed from E8s_v4 to E8s_v3

--- Size-Up-Hostpool.ps1
+++ Size-Up-Hostpool.ps1
@@ -3,7 +3,7 @@
 $resourceGroup = "WVD"
 $vmName = "KB-WVD-HPv4-0"
 
-##Re-Size VM to E8s_v4s
+##Re-Size VM to E8s_v3
 $vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
-$vm.HardwareProfile.VmSize = "E8s_v4"
+$vm.HardwareProfile.VmSize = "E8s_v3"
 Update-AzVM -VM $vm -ResourceGroupName $resourceGroup
