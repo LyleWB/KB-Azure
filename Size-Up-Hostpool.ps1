5a3a9d7852afde8a6f3698a095075a9743b987e9
LyleWB <82891371+LyleWB@users.noreply.github.com>
2021-09-06 17:58:03 +0100

    Update Size-Up-Hostpool.ps1
    Changed "KB-WVD-HPv4-0" to "KB-WVD-HPv4-1" inline with new VM name

--- Size-Up-Hostpool.ps1
+++ Size-Up-Hostpool.ps1
@@ -1,7 +1,7 @@
 #SCALE UP SIZE
 ##Kingston Burrowes Variables
 $resourceGroup = "WVD"
-$vmName = "KB-WVD-HPv4-0"
+$vmName = "KB-WVD-HPv4-1"
 
 ##Re-Size VM to E8s_v3
 $vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
